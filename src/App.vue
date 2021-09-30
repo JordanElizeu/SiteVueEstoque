c581fca28d4f83d2b8c1174f501404c17ac0550d
Andrew soares <andrewsoares347@gmail.com>
2021-09-30 19:57:34 -0300

    versao 1.1

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld/>
+      <Produtos/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
+//import HelloWorld from './components/HelloWorld';
+import Produtos from './views/produtos.vue'
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    Produtos
   },
 
   data: () => ({
---
+++ src/views/produtos.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-container>
+      <h2 class="text-center">Nossas vendas</h2>
+   <v-simple-table height="300px" >
+    <template>
+      <thead>
+        <tr>
+          <th class="text-left">
+            Produto
+          </th>
+          <th class="text-left">
+            Produto
+          </th>
+          <th class="text-left">
+            Valor
+          </th>
+          <th class="text-left">
+            Quantidade
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="item in desserts"
+          :key="item.id"
+          
+        >
+          <td>{{ item.data }}</td>
+          <td>{{ item.produto }}</td>
+          <td>{{ item.venda }}</td>
+          <td>{{ item.quantidade }}</td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
+  </v-container>
+</template>
+
+<script>
+export default {
+    name: 'produtos',
+    data: () => ({
+      desserts: [],
+    }),
+     created(){
+        fetch('https://jordanelizeu.github.io/Data/produtos.json')
+        .then(resultado => resultado.json())
+        .then(json => {
+            this.desserts = json
+        })
+    }
+}
+</script>
+
+<style>
+
+</style>
