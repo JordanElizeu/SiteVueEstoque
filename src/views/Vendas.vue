9d4f0359f4f2c7b2de004237a59c76136e3295d4
Heitor Moura <heitorandrade2001@gmail.com>
2021-09-30 20:10:44 -0300

    Consumindo API de Vendas

--- src/views/Vendas.vue
+++ src/views/Vendas.vue
@@ -1,15 +1,26 @@
 <template>
   <div class="vendas">
     <h1>Vendas</h1>
+    <div v-for="venda of vendas" class="lista" :key="venda.ID">
+      {{ venda.Nome_Produto }}
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Vendas'
+  name: 'Vendas',
+  data() {
+    return {
+      vendas: []
+    }
+  },
+  created() {
+    fetch('https://jordanelizeu.github.io/Data/vendas.json')
+      .then(response => response.json())
+      .then(json => {
+        this.vendas = json;
+      })
+  }
 }
 </script>
-
-<style>
-
-</style>
